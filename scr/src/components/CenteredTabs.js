7002816e1e5f2adeb4fabc92b14b8cd7543db756
manuelpope <manuelpope@gmail.com>
2021-02-10 13:20:31 -0300

    add buttons services

--- scr/src/components/CenteredTabs.js
+++ scr/src/components/CenteredTabs.js
@@ -3,7 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-
+import DropDownMenu from './DropDownMenu';
+import ListDividers from './Listdividers'
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
@@ -13,10 +14,31 @@ const useStyles = makeStyles({
 const CenteredTabs=()=> {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+  const[showServices,setShowServices] = React.useState(true);
+  const[showEmpresa,setShowEmpresa] = React.useState(true);
+  const[showfounders,setShowFounders] = React.useState(true);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const onClickServices =()=>{
+  setShowFounders(true);  
+  setShowEmpresa(true);
+    setShowServices(false);
+  };
+
+  const onClickEmpresa =()=>{
+    setShowFounders(true);  
+    setShowEmpresa(false);
+      setShowServices(true);
+    };
+
+    const onClickFounders =()=>{
+      setShowFounders(false);  
+      setShowEmpresa(true);
+        setShowServices(true);
+      };
 
   return (
     <Paper className={classes.root}>
@@ -27,11 +49,17 @@ const CenteredTabs=()=> {
         textColor="primary"
         centered
       >
-        <Tab label="Item One" />
-        <Tab label="Item Two" />
-        <Tab label="Item Three" />
+        <Tab label="Empresa" onClick={onClickEmpresa}/>
+        <Tab label="Servicios" onClick={onClickServices}/>
+        <Tab label="Founders" onClick={onClickFounders} />
       </Tabs>
+      <div>
+                  {!showServices &&  <ListDividers></ListDividers>}
+
+    </div>
     </Paper>
+   
+
   );
 }
 export default  CenteredTabs;
